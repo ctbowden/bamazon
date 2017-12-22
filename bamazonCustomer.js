7b5bd0cde642a15287846b7535ce57ca85d69a3b
Charlie Bowden <charles.bowden@gmail.com>
2017-12-22 01:12:20 -0500

    Working bamazon customer view

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,30 +9,54 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your Password
-  password: "password",
+  password: "",
   database: "bamazon"
 });
 
-readProducts();
+connection.connect(function(err){
+	if (err) throw err;
+	readProducts();
+})
 
-
-function bamazonUser() {
-	inquirer
-		.prompt({
-			name: "buyId"
-			type: "input"
-			message: "Please enter the Item_Id of the item you wish to purchase."
+var questions = [{
+			name: "buyId",
+			type: "input",
+			message: "Please enter the Item_Id of the item you wish to purchase.",
 			validate: function(value) {
 				if (isNaN(value) === false){
 					return true;
 				}
 				return false;
 			}
+		},
+		{
+			name: "qty",
+			type: "input",
+			message: "Please enter the Quantity you would like to purchase.",
+			validate: function(value) {
+				if (isNaN(value) === false){
+					return true;
+				}
+				return false;
+			}
+		}];
 
-		})
+var userItem = 0;
+var userQuantity = 0;
+var userPrice = 0;
+
+
+
+function bamazonUser() {
+	inquirer
+		.prompt(questions)
 		.then(function(answer){
-			// when finished prompting
+			// when finished prompting save answers
+			userItem = answer.buyId;
+			userQuantity = answer.qty;
 			
+			connection.query("SELECT * FROM products")
+
 		})
 }
 
@@ -40,6 +64,40 @@ function readProducts() {
   console.log("Displaying all BAMAZON has to offer!\n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-  });
+    // For Loop to Display All Items for Sale
+    for (var i = 0; i < res.length; i++) {
+    	console.log(
+    		"Item #: " + res[i].item_id +
+    		" || Name: " + res[i].product_name +
+    		" || Price: " + res[i].price +
+    		" || In Stock: " + res[i].stock_quantity
+    		);
+    	}
+
+    inquirer.prompt(questions).then(function(answer){
+    	
+    	var chosenItem = res[answer.buyId - 1];
+    	if (chosenItem.stock_quantity >= answer.qty){
+    		var remInv = chosenItem.stock_quantity - answer.qty;
+    		console.log("Fulfilling your order of ..." + chosenItem.product_name);
+    		connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: remInv}, {item_id: answer.buyId}], function(err,res){
+    			if (err) throw err;
+    			console.log("Inventory Updated.");
+    		});
+    		totalPrice(answer.qty, chosenItem.price);
+    	} else {
+    		console.log("Insufficient Quantity to fulfill that order.  We only have " + chosenItem.stock_quantity + " of that item available.  Please come again.");
+    		connection.end();
+    	}
+    })
+
+	})
+
+}
+
+
+function totalPrice(number, price) {
+	var total = parseFloat(number) * parseFloat(price);
+	console.log("Your total price today is: " + total);
+	connection.end();
+}
